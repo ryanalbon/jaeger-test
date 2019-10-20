032c6cfdff9e288a050735c64842848b8e510c00
Ryan Albon <ryan@insideboard.com>
2019-10-20 18:49:49 +0200

    Running jaeger-all-in-one via Docker Compose.

--- api/src/app.js
+++ api/src/app.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const jaegerClient = require('jaeger-client');
 
+const logger = require('./logger');
 const loggerMiddleware = require('./logger-middleware');
+const { name: serviceName } = require('../package.json');
 
+const tracerConfig = {
+  serviceName,
+  reporter: {
+    collectorEndpoint: 'http://jaeger:14268/api/traces',
+  },
+  sampler: {
+    type: 'const',
+    param: 1,
+  },
+};
+
+const tracer = jaegerClient.initTracer(tracerConfig, { logger });
 const app = express();
 
 app.use(loggerMiddleware);
 
 app.get('/status', (req, res) => res.status(200).end());
+
+app.get('/api/v1/people', (req, res) => {
+  const span = tracer.startSpan('getPeople');
+
+  res.status(200).json({ data: [{ name: 'Alice' }, { name: 'Bob' }] }).end();
+
+  span.finish();
+});
 
 module.exports = app;
--- api/src/logger.js
+++ api/src/logger.js
@@ -1,8 +1,8 @@
 const pino = require('pino');
 
-const { LOG_LEVEL } = require('./config');
+const { LOG_LEVEL: level } = require('./config');
 const { name: appName, version: appVersion } = require('../package.json');
 
-const logger = pino({ level: LOG_LEVEL }).child({ appName, appVersion });
+const logger = pino({ level }).child({ appName, appVersion });
 
 module.exports = logger;
